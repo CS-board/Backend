c68613b2939531a0022424b5f05124d228a8d048
jsj0010 <wjd020425@kumoh.ac.kr>
2026-01-23 12:10:55 +0900

    fix: CORS수정
    ## 변경사항
    도메인에 맞게 CORS수정

---
+++ src/main/java/com/chip/board/global/config/security/CorsProperties.java
@@ -0,0 +1,15 @@
+package com.chip.board.global.config.security;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.Collections;
+import java.util.List;
+
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "cs-board.cors")
+public class CorsProperties {
+    private List<String> origins = Collections.emptyList();
+}
